67043699e99faa5bb1b20acde9e3308949382e8b
Cellv2 <rich.a@live.com>
2018-10-29 22:46:49 +0000

    Add basic header and contact card styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Contact from "./components/Contact.js";
 import Header from "./components/Header.js";
 
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
@@ -10,8 +10,18 @@ class App extends Component {
         return (
             <div className="App">
                 <Header branding="Contact Manager" />
-                <Contact name="John Doe" email="jdoe@gmail.com" phone="12345-123-123" />
-                <Contact name="Karin Smith" email="ksmith@gmail.com" phone="09876-098-098" />
+                <div className="conatiner">
+                    <Contact
+                        name="John Doe"
+                        email="jdoe@gmail.com"
+                        phone="12345-123-123"
+                    />
+                    <Contact
+                        name="Karin Smith"
+                        email="ksmith@gmail.com"
+                        phone="09876-098-098"
+                    />
+                </div>
             </div>
         );
     }
