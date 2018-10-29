a071ba64a73b41ce58357f2b165180f85aeeadaf
Cellv2 <rich.a@live.com>
2018-10-29 21:08:12 +0000

    Add basic components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import Contact from "./components/Contact.js";
+import Header from "./components/Header.js";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>The App Component</h1>
+                <Header />
+                <Contact />
             </div>
         );
     }
