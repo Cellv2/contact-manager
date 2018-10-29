767b6b0042b1f6457e910d3c37e782ad01fb2fd5
Cellv2 <rich.a@live.com>
2018-10-29 22:21:14 +0000

    Add PropType examaple

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Header = props => {
     const { branding } = props;
@@ -11,5 +12,9 @@ const Header = props => {
 
 //if 'branding' isn't passed in, MyApp will be used instead
 Header.defaultProps = { branding: "MyApp" };
+
+Header.propTypes = {
+    branding: PropTypes.string.isRequired
+};
 
 export default Header;
