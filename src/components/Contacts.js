04794fb3fa219f34dd9b6d28de444014fa8337b0
Cellv2 <rich.a@live.com>
2018-10-29 23:00:06 +0000

    Not using constructor, so removing

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,31 +2,29 @@ import React, { Component } from "react";
 import Contact from "./Contact";
 
 export class Contacts extends Component {
-    constructor() {
-        super();
-        this.state = {
-            contacts: [
-                {
-                    id: 1,
-                    name: "John Doe",
-                    email: "jdoe@gmail.com",
-                    phone: "12345-123-123"
-                },
-                {
-                    id: 2,
-                    name: "Karin Smith",
-                    email: "ksmith@gmail.com",
-                    phone: "09876-098-098"
-                },
-                {
-                    id: 3,
-                    name: "Henry Wut",
-                    email: "Hwut@gmail.com",
-                    phone: "12345-098-098"
-                }
-            ]
-        };
-    }
+    state = {
+        contacts: [
+            {
+                id: 1,
+                name: "John Doe",
+                email: "jdoe@gmail.com",
+                phone: "12345-123-123"
+            },
+            {
+                id: 2,
+                name: "Karin Smith",
+                email: "ksmith@gmail.com",
+                phone: "09876-098-098"
+            },
+            {
+                id: 3,
+                name: "Henry Wut",
+                email: "Hwut@gmail.com",
+                phone: "12345-098-098"
+            }
+        ]
+    };
+
     render() {
         const { contacts } = this.state;
         return (
