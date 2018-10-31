eafe8e02d769595d599a5f0e516de42f15b095e9
Cellv2 <rich.a@live.com>
2018-10-31 23:03:41 +0000

    Add example of updating state to show/hide elements via click events

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,29 +2,35 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
-    state = {};
+    state = {
+        showContactInfo: false
+    };
 
-    onShowClick = (id, name, e) => {
-        console.log(id);
-        console.log(name);
+    onShowClick = e => {
+        this.setState({
+            showContactInfo: !this.state.showContactInfo
+        });
     };
 
     render() {
         const { name, email, phone } = this.props.contact;
+        const { showContactInfo } = this.state;
 
         return (
             <div className="card card-body mb-3">
                 <h4>
                     {name}{" "}
                     <i
-                        onClick={this.onShowClick.bind(this, "id1", name)}
+                        onClick={this.onShowClick}
                         className="fas fa-sort-down"
                     />
                 </h4>
-                <ul className="list-group">
-                    <li className="list-group-item">Email: {email}</li>
-                    <li className="list-group-item">Phone: {phone}</li>
-                </ul>
+                {showContactInfo ? (
+                    <ul className="list-group">
+                        <li className="list-group-item">Email: {email}</li>
+                        <li className="list-group-item">Phone: {phone}</li>
+                    </ul>
+                ) : null}
             </div>
         );
     }
