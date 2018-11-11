ea1a0f9e38f295f47ca961cedc0fe9e59cb276f8
Cellv2 <rich.a@live.com>
2018-11-11 20:56:17 +0000

    Add basic routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Contacts from "./components/contacts/Contacts.js";
 import AddContact from "./components/contacts/AddContact.js";
 import Header from "./components/layout/Header.js";
+import About from "./components/pages/About"
+
 import { Provider } from "./context";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,13 +14,18 @@ class App extends Component {
     render() {
         return (
             <Provider>
-                <div className="App">
-                    <Header branding="Contact Manager" />
-                    <div className="conatiner">
-                        <AddContact />
-                        <Contacts />
+                <Router>
+                    <div className="App">
+                        <Header branding="Contact Manager" />
+                        <div className="conatiner">
+                            <Switch>
+                                <Route exact path="/" component={Contacts} />
+                                <Route exact path="/contact/add" component={AddContact} />
+                                <Route exact path="/about" component={About} />
+                            </Switch>
+                        </div>
                     </div>
-                </div>
+                </Router>
             </Provider>
         );
     }
