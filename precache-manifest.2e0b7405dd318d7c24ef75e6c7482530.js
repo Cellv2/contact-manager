6100e1a54bc70eede113ee9f7808bd21e71eeeb8
Cellv2 <rich.a@live.com>
2018-11-25 09:18:01 +0000

    Updates

---
+++ precache-manifest.2e0b7405dd318d7c24ef75e6c7482530.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6bb11ecce176d3419b5",
+    "url": "/Cellv2/contact-manager/static/js/runtime~main.e6bb11ec.js"
+  },
+  {
+    "revision": "b2eb8fa6903fae3032e0",
+    "url": "/Cellv2/contact-manager/static/js/main.b2eb8fa6.chunk.js"
+  },
+  {
+    "revision": "2f21c5b9ed492a67a919",
+    "url": "/Cellv2/contact-manager/static/js/1.2f21c5b9.chunk.js"
+  },
+  {
+    "revision": "b2eb8fa6903fae3032e0",
+    "url": "/Cellv2/contact-manager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "2f21c5b9ed492a67a919",
+    "url": "/Cellv2/contact-manager/static/css/1.73ed22ba.chunk.css"
+  },
+  {
+    "revision": "4dddb5310fd4a7bb2dbf7ad3fc454fac",
+    "url": "/Cellv2/contact-manager/index.html"
+  }
+];
